3bca040eb3ed7dbca8aa5e8439d6cd446e69a022
jrmie <39067995+jrmie@users.noreply.github.com>
2020-03-24 23:59:01 +0100

    Add LaTeX template v0.1 to RStudio

--- R/formats.R
+++ R/formats.R
@@ -24,3 +24,26 @@ memento_html <- function() {
     collapsed = FALSE,
     theme = "journal")
 }
+
+memento_pdf <- function() {
+
+  # locations of resource files in the package
+  pkg_resource = function(...) {
+    system.file(..., package = "MementoTools")
+  }
+
+  theme_perso    = pkg_resource("rmarkdown/templates/formats/pdf/perso.theme")
+  template = pkg_resource("rmarkdown/templates/formats/pdf/default.latex")
+  logo = pkg_resource("rmarkdown/templates/formats/logo/memento.png")
+  arg_logo <- rmarkdown::pandoc_variable_arg("logo", logo)
+  arg_theme <- paste0("--highlight-style=", theme_perso)
+
+  # call the base html_document function
+  rmarkdown::pdf_document(
+    template = template,
+    pandoc_args = c(arg_logo, arg_theme),
+    toc = TRUE,
+    toc_depth = 3,
+    fig_width = 6.5,
+    fig_height = 4)
+}
